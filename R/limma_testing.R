5b21fe08ca16f2b2e80964c35407d08103e9f6c0
Marcus McHale <marcus.mchale@nuigalway.ie>
2021-12-07 12:06:33 +0000

    resolved question about normalisation and filtering

--- R/limma_testing.R
+++ R/limma_testing.R
@@ -4,25 +4,31 @@
 # Created on: 07/02/2020
 
 LimmaTesting <- R6::R6Class("LimmaTesting", list(
-	base_path = NULL,
+	pathing = NULL,
 	sample_details = NULL,
+	sample_details_source = NULL,
   	reference_details = NULL,
 	t2g = NULL,
 	dge_list = NULL,
 	txi_kallisto = NULL,
-	outliers = NULL,
-	initialize = function(pathing, sample_details, reference_details, outliers = NULL) {
-		self$base_path <- pathing$return_to_base()$append_to_path('quality_control')$current_path
-		self$sample_details <- sample_details
+	id = NULL,
+	initialize = function(pathing, sample_details, reference_details, id = "sample") {
+		if (!id %in% colnames(sample_details$s2c)) {
+			print(paste(id, "is not found in the sample details file. Consider supplying a custom value for id. Aborting"))
+			stop()
+		}
+	  	self$pathing <- pathing
+	  	self$sample_details_source <- sample_details
+		self$sample_details <- sample_details$clone()
 		self$reference_details <- reference_details
-	  	self$outliers <- outliers
+	  	self$id <- id
 		return(invisible(self))
 	},
 	tx_import = function(txi_kallisto = NULL) {
 	  	if (!(is.null(txi_kallisto))) {
 		 	self$txi_kallisto <- txi_kallisto
 		} else {
-			files <- file.path(self$sample_details$s2c$path, "abundance.h5")
+			files <- file.path(self$sample_details_source$s2c$path, "abundance.h5")
 			self$txi_kallisto <- tximport::tximport(
 				files,
 				type = 'kallisto',
@@ -34,78 +40,58 @@ LimmaTesting <- R6::R6Class("LimmaTesting", list(
 				countsFromAbundance = "lengthScaledTPM"
 			)
 		}
-		colnames(self$txi_kallisto$counts) <- self$sample_details$s2c$sample
 		return(invisible(self))
 	},
-	prepare_dgelist = function(group, ID=NULL) {
+	prepare_dgelist = function(group = NULL) {
+	  	if (!(group %in% colnames(self$sample_details$s2c))) {
+	  		stop('Grouping variable not found in sample details')
+	  	}
 	 	self$dge_list <- edgeR::DGEList(
-		  counts = self$txi_kallisto$counts[, !(colnames(self$txi_kallisto$counts) %in% self$outliers)],
+		  counts = self$txi_kallisto$counts,
 		  # here because we are looking at genes and not transcript isoforms
 			# I just take the mean estimate of effective length rather than considering any length bias
 		  genes = data.frame(rowMeans(self$txi_kallisto$length)),
-		  samples = self$sample_details$s2c[!(self$sample_details$s2c$sample %in% self$outliers),],
-		  group = self$sample_details$s2c[!(self$sample_details$s2c$sample %in% self$outliers), colnames(self$sample_details$s2c) == group]
+		  samples = self$sample_details_source$s2c,
+		  group = self$sample_details_source$s2c[, group]
 		)
-	  	if (!(is.null(ID))) {
-		  self$dge_list <- edgeR::sumTechReps(lt$dge_list, self$sample_details$s2c[, ID])
-		  all_same <- function(v) {
-			if(length(unique(v)) == 1) {
-			  TRUE
-			} else {
-			  FALSE
+		if (length(self$sample_details_source$s2c[,self$id]) != length(unique(self$sample_details_source$s2c[,self$id]))) {
+			print("Technical replicates will be summed.")
+			self$dge_list <- edgeR::sumTechReps(self$dge_list, self$sample_details_source$s2c[, self$id])
+			all_same <- function(v) {
+			  if(length(unique(v)) == 1) {
+				TRUE
+			  } else {
+				FALSE
+			  }
 			}
-		  }
-		  s2c_same <- self$sample_details$s2c %>%
-			dplyr::group_by(across(all_of(ID))) %>%
-		  	dplyr::summarise(across(.fns=all_same))
-		  keep <- unlist(apply(s2c_same, 2, all))
-		  keep[ID] <- T
-		  s2c <- self$sample_details$s2c %>%
-		  	dplyr::group_by(across(all_of(ID))) %>%
-			dplyr::slice(1) %>%
-			dplyr::select(which(keep)) %>%
-			dplyr::ungroup()
-		  if (!all(dim(s2c)==dim(self$sample_details$s2c))) {
-			print(paste("Summing technical replicates and removing rows where",  ID, "is duplicated"))
-			print(paste("Also removing columns that would be ambiguous, i.e. where summed replicates differ"))
-			self$sample_details$s2c <- s2c
-		  }
+			s2c_same <- self$sample_details_source$s2c %>%
+			  dplyr::group_by(across(all_of(self$id))) %>%
+			  dplyr::summarise(across(.fns=all_same))
+			keep_columns <- unlist(apply(s2c_same, 2, all))
+			keep_columns[self$id] <- T
+			self$sample_details$s2c <- self$sample_details_source$s2c %>%
+			  dplyr::group_by(across(all_of(self$id))) %>%
+			  dplyr::slice(1) %>%
+			  dplyr::select(which(keep_columns)) %>%
+			  dplyr::ungroup()
 		}
-	  	self$dge_list <- edgeR::calcNormFactors(self$dge_list) # default method is TMM
-	  	# i found in at least some cases that the norm factors affect filterbyexpr
-	    # seems more stringent to apply it after (less targets included)
-	    # this case was one where only one sample was replicated with two libraries, perhaps related to that
-	  	keep <- edgeR::filterByExpr(self$dge_list)
-	   	self$dge_list <- self$dge_list[keep, ]
-	   	return(invisible(self))
+	  	# self$dge_list <- edgeR::calcNormFactors(self$dge_list)
+	  	# best to not filter on normalised counts as TMM normalisation fails at low counts
+	    # see comment by Aaron Lun: https://support.bioconductor.org/p/116351/
+	   	self$dge_list <- self$dge_list[edgeR::filterByExpr(self$dge_list), ] %>%
+	  		edgeR::calcNormFactors() # renormalise after filter
+	    normalised_counts <- as.data.frame(edgeR::cpm(self$dge_list$counts)) %>%
+		  tibble::rownames_to_column("GeneID")
+		write.table(
+		  normalised_counts,
+		  file=file.path(pathing$base_path, paste0("cpm_filtered_across",fs::path_sanitize(self$id),".txt")),
+		  sep="\t",
+		  row.names = F
+		)
 	},
 	design = function(model) {
-	  	s2c <- dplyr::filter(self$sample_details$s2c, !(sample %in% self$outliers))
-	  	design <- model.matrix(model, s2c)
+	  	design <- model.matrix(model, self$sample_details$s2c)
 	  	return(design)
-	},
-	fit = function(model, block = NULL) {
-	  	design <- self$design(model)
-	  	voom_object <- limma::voom(self$dge_list, design)
-	  	if (is.null(block)) {
-		  	fit <- limma::lmFit(voom_object, design)
-		} else {
-		  	blocking_vector <- sample_details$s2c[
-			  !(self$sample_details$s2c$sample %in% self$outliers),
-			  colnames(self$sample_details$s2c) == block
-			]
-		  	cor <- self$calculate_correlation(voom_object, design, blocking_vector)
-		  	fit <- limma::lmFit(
-			  voom_object,
-			  block = blocking_vector,
-			  correlation = cor
-			)
-		}
-		return(limma::eBayes(fit))
-	},
-	contrast_fit = function(fit, contrasts) {
-	  	fit <- limma::contrasts.fit(fit, contrasts)
-	  	return(limma::eBayes(fit))
 	},
 	calculate_correlation =function (voom_object, design, block) {
 		corfit <- limma::duplicateCorrelation(
@@ -116,7 +102,28 @@ LimmaTesting <- R6::R6Class("LimmaTesting", list(
 	  	print(paste('Estimated average correlation between block members:', corfit$cor))
 	  	return(corfit$cor)
 	},
-
+	fit = function(model, block = NULL) {
+	  	design <- self$design(model)
+	  	voom_object <- limma::voom(self$dge_list, design)
+	  	if (is.null(block)) {
+		  	fit <- limma::lmFit(voom_object, design)
+		} else {
+		  	blocking_vector <- self$sample_details$s2c[, block]
+		  	cor <- self$calculate_correlation(voom_object, design, blocking_vector)
+		  	fit <- limma::lmFit(
+			  voom_object,
+			  block = blocking_vector,
+			  correlation = cor
+			)
+		}
+	  	fit <- limma::eBayes(fit)
+		return(fit)
+	},
+	contrast_fit = function(model, contrasts, block = NULL) {
+	  fit <- self$fit(model, block)
+	  contrast_fit <- limma::eBayes(limma::contrasts.fit(fit, contrasts))
+	  return(contrast_fit)
+	},
 	deg_results = function(fit, coef_index, n = NULL) {
 	  	# coeff_index is the indexed position of the cofficient of interest in the model,
 	  	# i.e. which collumn in model.matrix
@@ -153,4 +160,5 @@ LimmaTesting <- R6::R6Class("LimmaTesting", list(
 			ggplot2::geom_vline(xintercept = 0, colour = "black", linetype = "longdash")
 		ggplot2::ggsave(file_path, plot=p)
 	}
+
 ))
